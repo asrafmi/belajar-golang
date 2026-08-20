3e01032e5f0a85357e21dac4d58afec4d57ed053
asrafmi <asraf.muhammad07@gmail.com>
2026-08-21 06:53:12 +0700

    Refactor arithmetic tests to use subtests for better organization and clarity

--- unit-test/helper/helper_test.go
+++ unit-test/helper/helper_test.go
@@ -15,30 +15,29 @@ func TestMain(m *testing.M) {
 	fmt.Println("After running all test in this package")
 }
 
-func TestAddPositiveInt(t *testing.T) {
-	result := Add(1, 2)
-	if result != 3 {
-		t.Error("Result of add operation should 3")
-	}
-}
-
-func TestAddPositiveIntAssert(t *testing.T) {
-	result := Add(1, 2)
-	assert.Equal(t, 3, result, "Result must be 3")
-	fmt.Println("Test executed") // assert using t.Fail, so it wont break the codes below
-}
-
-func TestAddPositiveIntRequire(t *testing.T) {
-	result := Add(1, 2)
-	require.Equal(t, 3, result, "Result must be 3")
-	fmt.Println("Test executed") // assert using t.FailNow, so it will break the codes below
-}
-
-func TestAddNegativeInt(t *testing.T) {
-	result := Add(-1, 2)
-	if result != 1 {
-		t.Fatal("Result of add operation should 1")
-	}
+func TestAdd(t *testing.T) {
+	t.Run("PositiveInt", func(t *testing.T) {
+		result := Add(1, 2)
+		if result != 3 {
+			t.Error("Result of add operation should 3")
+		}
+	})
+	t.Run("PositiveIntAssert", func(t *testing.T) {
+		result := Add(1, 2)
+		assert.Equal(t, 3, result, "Result must be 3")
+		fmt.Println("Test executed") // assert using t.Fail, so it wont break the codes below
+	})
+	t.Run("PositiveIntRequire", func(t *testing.T) {
+		result := Add(1, 2)
+		require.Equal(t, 3, result, "Result must be 3")
+		fmt.Println("Test executed") // require using t.FailNow, so it will break the codes below
+	})
+	t.Run("NegativeInt", func(t *testing.T) {
+		result := Add(-1, 2)
+		if result != 1 {
+			t.Fatal("Result of add operation should 1")
+		}
+	})
 }
 
 func TestSkip(t *testing.T) {
@@ -50,42 +49,46 @@ func TestSkip(t *testing.T) {
 	assert.Equal(t, 3, result, "Result must be 3")
 }
 
-func TestMinusPositiveIntAssert(t *testing.T) {
-	result := Minus(2, 1)
-	assert.Equal(t, 1, result, "Result must be 1")
+func TestMinus(t *testing.T) {
+	t.Run("PositiveIntAssert", func(t *testing.T) {
+		result := Minus(2, 1)
+		assert.Equal(t, 1, result, "Result must be 1")
+	})
+
+	t.Run("NegativeIntAssert", func(t *testing.T) {
+		result := Minus(-2, 1)
+		assert.Equal(t, -3, result, "Result must be -3")
+	})
+
+	t.Run("PositiveFloatAssert", func(t *testing.T) {
+		result := Minus(2.5, 1)
+		assert.Equal(t, 1.5, result, "Result must be 1.5")
+	})
+
+	t.Run("NegativeFloatAssert", func(t *testing.T) {
+		result := Minus(-2.5, 1)
+		assert.Equal(t, -3.5, result, "Result must be -3.5")
+	})
 }
 
-func TestMinusNegativeIntAssert(t *testing.T) {
-	result := Minus(-2, 1)
-	assert.Equal(t, -3, result, "Result must be -3")
-}
+func TestMultiply(t *testing.T) {
+	t.Run("PositiveIntAssert", func(t *testing.T) {
+		result := Multiply(2, 1)
+		assert.Equal(t, 2, result, "Result must be 2")
+	})
 
-func TestMinusPositiveFloatAssert(t *testing.T) {
-	result := Minus(2.5, 1)
-	assert.Equal(t, 1.5, result, "Result must be 1.5")
-}
+	t.Run("NegativeIntAssert", func(t *testing.T) {
+		result := Multiply(-2, 1)
+		assert.Equal(t, -2, result, "Result must be -2")
+	})
 
-func TestMinusNegativeFloatAssert(t *testing.T) {
-	result := Minus(-2.5, 1)
-	assert.Equal(t, -3.5, result, "Result must be -3.5")
-}
+	t.Run("PositiveFloatAssert", func(t *testing.T) {
+		result := Multiply(2.5, 1)
+		assert.Equal(t, 2.5, result, "Result must be 2.5")
+	})
 
-func TestMultiplyPositiveIntAssert(t *testing.T) {
-	result := Multiply(2, 1)
-	assert.Equal(t, 2, result, "Result must be 2")
-}
-
-func TestMultiplyNegativeIntAssert(t *testing.T) {
-	result := Multiply(-2, 1)
-	assert.Equal(t, -2, result, "Result must be -2")
-}
-
-func TestMultiplyPositiveFloatAssert(t *testing.T) {
-	result := Multiply(2.5, 1)
-	assert.Equal(t, 2.5, result, "Result must be 2.5")
-}
-
-func TestMultiplyNegativeFloatAssert(t *testing.T) {
-	result := Multiply(-2.5, 1)
-	assert.Equal(t, -2.5, result, "Result must be -2.5")
+	t.Run("NegativeFloatAssert", func(t *testing.T) {
+		result := Multiply(-2.5, 1)
+		assert.Equal(t, -2.5, result, "Result must be -2.5")
+	})
 }
