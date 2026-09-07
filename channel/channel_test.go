b67cfdfc7da6772b9713aa48224cefba15d6769d
asrafmi <asraf.muhammad07@gmail.com>
2026-09-08 01:53:53 +0700

    Add TestSelectChannel to demonstrate channel selection with goroutines

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -142,3 +142,30 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Done")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1, "Channel 1")
+	go GiveMeResponse(channel2, "Channel 2")
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari: ", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari: ", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+}
