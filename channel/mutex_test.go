7908d284b3b4597f2ed39118081ac53a2153c5de
asrafmi <asraf.muhammad07@gmail.com>
2026-09-16 21:22:29 +0700

    Add RWMutex implementation and tests for concurrent balance management

--- channel/mutex_test.go
+++ channel/mutex_test.go
@@ -29,4 +29,31 @@ type BankAccount struct {
 	Balance int
 }
 
-func AddBalance(account *BankAccount)
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance: ", account.Balance) // output: 10000 (100x100)
+}
